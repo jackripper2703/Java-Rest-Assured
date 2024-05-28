01c9a86b2688dd0ce2cc8528bfc17b88462a4a80
Konstantin <69685027+jackripper2703@users.noreply.github.com>
2024-05-29 00:48:15 +0300

    /

--- src/test/java/tests/BaseApiTest.java
+++ src/test/java/tests/BaseApiTest.java
@@ -7,17 +7,25 @@ import RestAssured.models.FullUser;
 import RestAssured.services.FileService;
 import RestAssured.services.UserService;
 import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static RestAssured.helper.ConfigProvider.URL;
 import static RestAssured.helper.RandomTestData.getRandomUser;
+import static org.hamcrest.Matchers.*;
 
 @ExtendWith({LoginExtension.class,AdminUserResolver.class})
 
 public class BaseApiTest {
+
+    // Создаем спецификацию для ответов
+    static ResponseSpecification responseSpec = new ResponseSpecBuilder()
+            .expectResponseTime(lessThan(5000L))  // время в миллисекундах
+            .build();
 
     protected static UserService userService;
     protected static FileService fileService;
@@ -25,6 +33,7 @@ public class BaseApiTest {
 
     @BeforeAll
     public static void setUp() {
+        RestAssured.responseSpecification = responseSpec;
         RestAssured.baseURI = URL;
         RestAssured.filters(
                 new RequestLoggingFilter(),
